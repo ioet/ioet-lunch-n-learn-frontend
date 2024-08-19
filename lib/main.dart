0d629907665abdba563ddb208ef5371dc7c2202d
Alan1108 <alan.ibm1108@gmail.com>
2024-08-19 01:41:03 -0500

    feat/AB(wip): Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -55,17 +58,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void loginWithGoogle() {
+    //TO-DO: Login function
   }
 
   @override
@@ -77,49 +71,83 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            "assets/images/background.svg",
+            colorFilter: const ColorFilter.mode(
+                Color.fromARGB(255, 22, 22, 22), BlendMode.srcIn),
+          ),
+          Stack(
+            children: [
+              const Align(
+                alignment: Alignment(-.75, 0),
+                child: Image(
+                  image: AssetImage("assets/images/ioetLogoLeft.png"),
+                  width: 150,
+                  height: 150,
+                ),
+              ),
+              Align(alignment: Alignment.centerRight, child: _ioetImageHalf())
+            ],
+          ),
+          Expanded(
+              flex: 1,
+              child: Positioned.fill(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                    width: 225,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: loginWithGoogle,
+                      child: Row(
+                        children: [
+                          SvgPicture.network(
+                              "https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg"),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                            child: const Text(
+                              "Sign in with Google",
+                              style: TextStyle(
+                                  fontFamily: "Barlow", color: Colors.black),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              )))
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  Widget _ioetImageHalf() {
+    return ClipRect(
+      child: Align(
+        alignment: Alignment.centerLeft,
+        widthFactor: .5,
+        child: Stack(
+          children: [
+            ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Image.asset(
+                "assets/images/ioetLogo.png",
+                width: 500,
+                height: 500,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Image.asset(
+              "assets/images/ioetLogo.png",
+              width: 500,
+              height: 500,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
